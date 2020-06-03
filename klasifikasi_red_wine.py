3df96ff5afea973ff22840e73cbb3f942be8c12d
Ananda Kusumawardana <ananda.kusumawardana@ti.ukdw.ac.id>
2020-06-03 14:10:15 +0700

    Add Graphic Charts and Read The CSV

--- klasifikasi_red_wine.py
+++ klasifikasi_red_wine.py
@@ -9,4 +9,39 @@ from sklearn.model_selection import train_test_split
 
 doc = "winequality-red.csv"
 dataset = pd.read_csv(doc, sep=';', header=0)
-#print(dataset)
+print(dataset)
+
+# Memperlihatkan 5 data teratas
+dataset.head()
+
+
+dataset.info()
+
+# Grafik
+
+fig = plt.figure(figsize = (10,6))
+sns.barplot(x = 'quality', y = 'fixed acidity', data = dataset)
+
+fig = plt.figure(figsize = (10,6))
+sns.barplot(x = 'quality', y = 'volatile acidity', data = dataset)
+
+fig = plt.figure(figsize = (10,6))
+sns.barplot(x = 'quality', y = 'citric acid', data = dataset)
+
+fig = plt.figure(figsize = (10,6))
+sns.barplot(x = 'quality', y = 'residual sugar', data = dataset)
+
+fig = plt.figure(figsize = (10,6))
+sns.barplot(x = 'quality', y = 'chlorides', data = dataset)
+
+fig = plt.figure(figsize = (10,6))
+sns.barplot(x = 'quality', y = 'free sulfur dioxide', data = dataset)
+
+fig = plt.figure(figsize = (10,6))
+sns.barplot(x = 'quality', y = 'total sulfur dioxide', data = dataset)
+
+fig = plt.figure(figsize = (10,6))
+sns.barplot(x = 'quality', y = 'sulphates', data = dataset)
+
+fig = plt.figure(figsize = (10,6))
+sns.barplot(x = 'quality', y = 'alcohol', data = dataset)
