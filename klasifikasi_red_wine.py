c67e0fce98b5172bce15992759a9e4208a8bc31c
Valent Christian Adiputra <51280887+valentcap@users.noreply.github.com>
2020-06-04 07:22:32 +0700

    Good and bad

--- klasifikasi_red_wine.py
+++ klasifikasi_red_wine.py
@@ -17,8 +17,9 @@ dataset.head()
 
 dataset.info()
 
-# Grafik
+# Grafik pengaruh variabel terhadap kualitas
 
+#fixed acidity
 fig = plt.figure(figsize = (10,6))
 sns.barplot(x = 'quality', y = 'fixed acidity', data = dataset)
 
@@ -45,3 +46,36 @@ sns.barplot(x = 'quality', y = 'sulphates', data = dataset)
 
 fig = plt.figure(figsize = (10,6))
 sns.barplot(x = 'quality', y = 'alcohol', data = dataset)
+
+# Binary Classification
+# Wine dengan quality diatas 6.5 dianggap sebagai good(1) dan dibawahnya bad(0)
+bins = (2, 6.5, 8)
+group_names = ['bad', 'good']
+dataset['quality'] = pd.cut(dataset['quality'], bins=bins, labels=group_names)
+
+label_quality = LabelEncoder()
+
+dataset['quality'] = label_quality.fit_transform(dataset['quality'])
+
+
+sns.countplot(dataset['quality'])
+
+X = dataset.drop('quality', axis=1)
+
+dataset['quality'].value_counts()
+
+# Training data
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+sc = StandardScaler()
+
+X_train = sc.fit_transform(X_train)
+X_test = sc.fit_transform(X_test)
+
+# SVC
+svc = SVC(gamma='auto')
+svc.fit(X_train, y_train)
+pred_svc = svc.predict(X_test)
+
+print(classification_report(y_test, pred_svc))
+y = dataset['quality']
